036c59e1752be365924082614fc8790238936456
Eric Collard <ruffyreborn@gmail.com>
2023-03-27 08:42:48 -0500

    Uploading assignment

---
+++ script.js
@@ -0,0 +1,37 @@
+const jedi = ['Luke'];
+
+console.log(jedi.push('Obi-Wan Kenobi'));
+
+console.log(jedi.unshift('Yoda'));
+
+console.log(jedi.slice(1, 2));
+
+console.log(jedi.pop());
+
+console.log(jedi.shift());
+
+console.log(jedi);
+
+
+const sithLords = ['Darth Vader', 'Darth Sidious', 'Darth Maul'];
+
+const imperialOfficers = ['Grand Moff Tarkin', 'Orson Krennic'];
+
+starWarsVillains = sithLords.concat(imperialOfficers)
+
+console.log(starWarsVillains);
+
+console.log(starWarsVillains.slice(0, 2));
+
+
+const droids = {
+    astromech: 'R2-D2',
+    protocol: 'C-3PO',
+    assassin: 'IG-88',
+}
+
+console.log(droids['astromech']);
+
+console.log(droids.protocol);
+
+console.log(dro
