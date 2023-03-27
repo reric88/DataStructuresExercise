65618bbc9e5b4e5c958999e6ba0d9f77c3601b7a
Eric Collard <ruffyreborn@gmail.com>
2023-03-27 08:48:03 -0500

    added question comments

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
+// 1a
+// 1b
 const jedi = ['Luke'];
 
+// 1c
 console.log(jedi.push('Obi-Wan Kenobi'));
 
+// 1d
 console.log(jedi.unshift('Yoda'));
 
+// 1e
 console.log(jedi.slice(1, 2));
 
+// 1f
 console.log(jedi.pop());
 
+// 1g
 console.log(jedi.shift());
-
 console.log(jedi);
 
-
+// 2a
 const sithLords = ['Darth Vader', 'Darth Sidious', 'Darth Maul'];
 
+// 2b
 const imperialOfficers = ['Grand Moff Tarkin', 'Orson Krennic'];
 
+// 2c
 starWarsVillains = sithLords.concat(imperialOfficers)
 
 console.log(starWarsVillains);
 
+// 2d
 console.log(starWarsVillains.slice(0, 2));
 
-
+// 3a
 const droids = {
     astromech: 'R2-D2',
     protocol: 'C-3PO',
     assassin: 'IG-88',
 }
 
+// 3b
 console.log(droids['astromech']);
 
 console.log(droids.protocol);
 
+// 3c
 console.log(dro
